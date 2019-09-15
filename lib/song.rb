9e7d4a50fd292965850cdc2c09cbe4f572d89593
arnie b <arnold.bwaila@gmail.com>
2019-09-15 16:33:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,38 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+     @@all << self.new
+    
+  end
+  
+  def self.new_by_name(nme)
+      sng = self.new
+      sng.name = nme
+      sng
+  end
+  
+  def self.create_by_name(nme)
+       sng = self.new
+       sng.name = nme
+       @@all << sng
+       sng
+  end
+  
+  def self.find_by_name(nme)
+      @@all.each do|sng| 
+        if sng.name == nme
+           return sng
+        else return "not found"
+        end
+      end
+  end
+  
+  def self.find_or_create_by_name(nme)
+      if Song.find_by_name
+  end
+  
   def self.all
     @@all
   end
@@ -9,5 +40,5 @@ class Song
   def save
     self.class.all << self
   end
-
 end
+
